98401d58036841582ce8a554a5d5b303b4f1b1a6
prithvihttp <prithviraj6868@gmail.com>
2025-07-31 19:11:49 +0530

    first commit

---
+++ Project1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./Components/Hero.jsx";
+import Navigation from "./Components/Navigation.jsx";
+
+const App = () => {
+  return (
+    <div>
+   <Navigation/>
+   <HeroSection/>
+  </div>
+  )
+}
+export default App
