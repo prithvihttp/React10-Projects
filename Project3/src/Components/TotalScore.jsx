2f978b2c0c1477bd8041352d401c3ee4b50e11cd
prithvihttp <prithviraj6868@gmail.com>
2025-08-05 22:30:26 +0530

    Total Score

--- Project3/src/Components/TotalScore.jsx
+++ Project3/src/Components/TotalScore.jsx
@@ -1,35 +1,25 @@
 import styled from "styled-components";
 
-const Rules = () => {
+const TotalScore = ({ score }) => {
   return (
-    <RulesContainer>
-      <h2>How to play dice game</h2>
-      <div className="text">
-        <p>Select any number</p>
-        <p>Click on dice image</p>
-        <p>
-          after click on dice if selected number is equal to dice number you
-          will get same point as dice{" "}
-        </p>
-        <p>if you get wrong guess then 2 point will be dedcuted </p>
-      </div>
-    </RulesContainer>
+    <ScoreContainer>
+      <h1>{score}</h1>
+      <p>Total Score</p>
+    </ScoreContainer>
   );
 };
 
-export default Rules;
+export default TotalScore;
 
-const RulesContainer = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  background-color: #fbf1f1;
-  padding: 20px;
-  margin-top: 40px;
-  border-radius: 10px;
-  h2 {
-    font-size: 24px;
+const ScoreContainer = styled.div`
+  max-width: 200px;
+  text-align: center;
+  h1 {
+    font-size: 100px;
+    line-height: 100px;
   }
-  .text {
-    margin-top: 24px;
+  p {
+    font-size: 24px;
+    font-weight: 500px;
   }
 `;
